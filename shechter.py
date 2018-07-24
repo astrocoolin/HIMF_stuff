19aae6c2a65c6589211edcec91bc5fa7b57ba643
astrocoolin <colin.n.lewis@gmail.com>
2018-07-24 17:14:46 -0400

    with exponential disk

--- shechter.py
+++ shechter.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 
 def func(x,a,b,c,d):
+    #exponential fit for V0, rPE
     return a*np.exp(-x*b+c) +d
 
 def func2(x,a,b,c):
+    #quadratic fit for A
     return a*x**2. + b*x + c
 
 def make_vrot(radi,Mag,hr):
@@ -29,29 +31,7 @@ def make_vrot(radi,Mag,hr):
 
     return vt*(1.-np.exp(-radi/rt))*(1.+a*radi/rt)
 
-def phi(MHI, Mstar, alpha, phi_0):
-    return np.log(10.) *phi_0* (MHI/Mstar)**(alpha+1.) * np.exp(-MHI/Mstar)
-
-def DHI(MHI, slope,intercept):
-    return (10.**(slope*np.log10(MHI)+intercept))
-
-def MBar_calc(Mgas,slope,const,split):
-    MBar = np.zeros_like(MHI)
-    for i, mass in enumerate(np.log10(MHI)):
-        if mass < split:
-            MBar[i] = mass * 1./slope[0,0] - const[0,0]/slope[0,0]
-        else:
-            MBar[i] = mass * 1./slope[1,0] - const[1,0]/slope[1,0]
-    return 10.** MBar       
-
-
-def BTFR(Mbar,slope,const):
-    logv = np.log10(Mbar) * 1./slope[0] - const[0]/slope[0]
-    #print(np.log10(Mbar))
-    #print(10.**logv)
-    return 10.**(logv)
-
-def expdisk(R1,Mass,i):
+def HI_profile(R1,Mass,i):
     #Have to find Sigma_centre
     #Integrated Flux should give you the mass of the galaxy
     b = np.zeros_like(R1)
@@ -80,64 +60,100 @@ def expdisk(R1,Mass,i):
 
     Rs   = (R1)*0.18
     Fluxc = np.exp((R1 - b)/Rs) 
-    #print('Mass='+str(round(MGuess_i/Mass[i]*100,2))+'%. Mass outside '+ str(out/R)+ '*R1= ' +str(round(edge_i/Mass[i]*100.,2)),'%.', 'b='+str(round(b[i]/R1[i],2)))
-
+    print(str(round(MGuess_i/Mass[i],2)*100.)+'% of the mass')
     return Rs, Fluxc,b
+
+#def expdisk(slope,const,Mass):
+#    #expdisk for the polyex fit
+#    return 10.**(slope * (np.log10(Mass)-10.) + const)
 
 
 ####################################
 MHI = np.round(10.**(np.arange(4.,12.1,.1)),1)
 i=input('Input HI Mass (dex):\n')
-print(10.**float(i))
-#i=np.where(MHI==round(10.**float(i),0))
-#i=np.where(float(i)==np.log10(MHI))
+i=10.**float(i)
 i = np.array(np.argmin(abs(float(i)-MHI)))
-print('HI Mass:',MHI[i]*u.Msun)
+print('HI Mass:','{:.4}'.format(MHI[i]*u.Msun))
 ####################################
 # Martin et al 2010 
 # https://arxiv.org/abs/1008.5107
 phi_0           = 0.0048 #\pm 0.3E-3
 Mstar           = 10.**9.96 #\pm 0.02 dex
 alpha           = -1.33 #\pm 0.02
+
+def phi(MHI, Mstar, alpha, phi_0):
+    #Mass Function
+    return np.log(10.) *phi_0* (MHI/Mstar)**(alpha+1.) * np.exp(-MHI/Mstar)
+
 HIMF = phi(MHI,Mstar,alpha,phi_0)
 # MPC^-3 dex^-1
 # print(10.**HIMF)
 ####################################
 # Jing Wang, Koribalski, et al 2016
 # https://arxiv.org/abs/1605.01489
-DHI_slope       =   0.506
-DHI_intercept   =  -3.293
-DHI = DHI(MHI,DHI_slope, DHI_intercept)
-print('HI Radius:',DHI[i]*u.kpc)
+slope       =   0.506
+const       =  -3.293
+
+def DHI(MHI, slope,intercept):
+    #Diameter-Mass relation
+    return (10.**(slope*np.log10(MHI)+intercept))
+
+DHI = DHI(MHI,slope, const)
+print('HI Radius:','{:.4}'.format(DHI[i]*u.kpc))
 ####################################
 # Bradford et al 2015, right after eq 4
 # https://arxiv.org/abs/1505.04819
 split           = 9.2832
 Mgas            = MHI * 1.4
+
+def MBar_calc(Mgas,slope,const,split):
+    #Baryonic Mass calculator
+    MBar = np.zeros_like(MHI)
+    for i, mass in enumerate(np.log10(MHI)):
+        if mass < split:
+            MBar[i] = mass * 1./slope[0,0] - const[0,0]/slope[0,0]
+        else:
+            MBar[i] = mass * 1./slope[1,0] - const[1,0]/slope[1,0]
+    return 10.** MBar       
+
 slope = np.array([[1.052,0.058],[0.461,0.011]])
 const = np.array([[0.236,0.476],[5.329,0.112]])
-Mbar = MBar_calc(Mgas,slope,const,split) + MHI
-print('Baryonic Mass:',Mbar[i]*u.Msun)
+Mbar = MBar_calc(Mgas,slope,const,split) + Mgas
+print('Baryonic Mass:','{:.4}'.format(Mbar[i]*u.Msun))
 ####################################
-# Ouellette et al 2017
-# https://arxiv.org/abs/1705.10794
-# slope = np.array([3.57,0.16])
-# const = np.array([2.44,0.35])
-# This one seems to fit better:
 # Lelli et al 2015
 # https://arxiv.org/abs/1512.04543
+
+def BTFR(Mbar,slope,const):
+    #Baryonic Tully Fisher
+    logv = np.log10(Mbar) * 1./slope[0] - const[0]/slope[0]
+    return 10.**(logv)
+
 slope = np.array([3.71,0.08])
 const = np.array([2.27,0.18])
 v = BTFR(Mbar,slope,const)
-print('Vflat:',v[i]*u.km/u.s)
+print('Vflat:','{:.4}'.format(v[i]*u.km/u.s))
 ####################################
 # Jing et al 2014
 # https://arxiv.org/abs/1401.8164
 # scale length = 0.18 RHI
-Rs, Sig0, b = expdisk(DHI/2.,MHI,i)
-print('HI Scale length:',Rs[i]*u.kpc)
-print('Central density:',Sig0[i]*u.Msun/u.pc**2.)
+Rs, Sig0, b = HI_profile(DHI/2.,MHI,i)
+print('HI Scale length:','{:.4}'.format(Rs[i]*u.kpc))
+print('Central density:','{:.4}'.format(Sig0[i]*u.Msun/u.pc**2.))
+####################################
+# Wu 2017
+# https://arxiv.org/abs/1710.06440
+slope = np.array([0.385,0.013])
+const = np.array([0.281,0.010])
 
+def expdisk(slope,const,Mass):
+    #expdisk for the polyex fit
+    return 10.**(slope * (np.log10(Mass)-10.) + const)
+
+Rd = expdisk(slope[0],const[0],MHI)
+print('Optical Scale length:','{:.4}'.format(Rd[i]*u.kpc))
+
+###################################
 radi = np.arange(0.,DHI[i],0.001)
 sbr = np.zeros_like(radi)
 for j, r in enumerate(radi):
@@ -148,30 +164,3 @@ for j, r in enumerate(radi):
     else:
         sbr[j] = (Sig0[i]/np.exp(-b[i]/Rs[i])) * np.exp(-r/Rs[i])
 
-plt.semilogy(radi/(DHI[i]/2.),sbr)
-plt.axvline(1.)
-plt.axhline(1.)
-plt.title(str(np.log10(MHI[i]))+' dex Solar Massses of HI')
-plt.xlabel('R / R1,  R1 ='+str(DHI[i]/2.)+' kpc')
-plt.ylabel('HI Mass Density [Msun pc$^{-2}$]')
-plt.xlim(0,2.)
-plt.ylim(0.2,10)
-plt.show()
-#
-plt.plot(np.log10(MHI),np.log10(HIMF))
-plt.xlim(6,11)
-plt.ylim(-6,0)
-#plt.show()
-
-plt.plot(np.log10(MHI),np.log10(DHI))
-#plt.show()
-
-plt.plot(np.log10(MHI),np.log10(Mbar))
-#plt.show()
-
-plt.plot(np.log10(Mbar),np.log10(v))
-#plt.show()
-
-#print(np.log10(HIMF))
-#print(np.log10(MHI))
-#print(np.log10(v))
