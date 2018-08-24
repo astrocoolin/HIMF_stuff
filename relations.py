e55ae557b7e940fbff514e6c9d1a534af9b18e6a
astrocoolin <colin.n.lewis@gmail.com>
2018-08-24 15:45:13 -0400

    cleaned up, have to fix a bug

--- relations.py
+++ relations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib as mpl
+import scipy as sp
 #mpl.use('Agg')
 import matplotlib.pyplot as plt
 import astropy.units as u
@@ -8,20 +9,26 @@ from scipy.optimize import curve_fit
 from scipy import integrate
 from Input_output import rothead
 
+sp.special.seterr()
+
+
 def func(x,a,b,c,d):
-    #exponential fit for V0, rPE
+    # Fit for V0
     return a*np.exp(-x*b-c) +d*x
 
 def func2(x,b,c):
-    #Linear fit for A, Rt
+    # Linear fit for A
     return b*x + c
 
-def func3(x,a,b,c,d,e):
-    #exponential fit for V0, rPE
-    #return  -a*np.exp(x*b-c) -d*x
+def func3(x,a,b):
+    # Fit for rPE
     return a*np.log10(-x) - b
 
-def make_vrot(radi,Mag,Rd,v,RHI,mstar):
+def make_vrot(radi,Mag,Ropt,alpha):
+    # Returns a polyex rotation curve
+    # Input a magnitude, Ropt, radii, and alpha
+    # Catinella et al 2007
+    # https://arxiv.org/abs/astro-ph/0512051
     V0 = np.array([275.,255.,225.,200.,170.,148.,141.,122.,103.,85.])
     dV0 = np.array([6.,2.,1.,1.,1.,2.,2.,2.,2.,5.])
     A=np.array([0.008,0.002,0.003,0.002,0.011,0.022,0.010,0.020,0.029,0.019])
@@ -36,16 +43,17 @@ def make_vrot(radi,Mag,Rd,v,RHI,mstar):
 
     vt=func(Mag,*V0)
     a=func2(Mag,*A)
-    rt=Rd*func3(Mag,*rPE) 
+    rt=Ropt*func3(Mag,*rPE) 
 
     return vt*(1.-np.exp(-radi/rt))*(1.+a*radi/rt),rt
 
-def Magcalc(vrot,Rd,RHI,mstar):
+def Magcalc(vrot,Ropt,RHI,mstar):
     # Find Mag, and slope based on
     # Catinella et al 2007
     # https://arxiv.org/abs/astro-ph/0512051
     # Dutton et al 2018
     # https://arxiv.org/abs/1807.10518
+
     A_=np.array([0.008,0.002,0.003,0.002,0.011,0.022,0.010,0.020,0.029,0.019])
     dA = np.array([0.003,0.001,0.001,0.001,0.001,0.002,0.003,0.005,0.005,0.015])
     rPE_ = np.array([0.126,0.132,0.149,0.164,0.178,0.201,0.244,0.261,0.260,0.301])
@@ -63,119 +71,102 @@ def Magcalc(vrot,Rd,RHI,mstar):
     Mag = np.arange(-25,0.,0.001)
     a=func2(Mag,*A)
     vt_0=func(Mag,*V0)
-    rt=Rd*func3(Mag,*rPE)
+    rt=Ropt*func3(Mag,*rPE)
 
-    if False:
-        plt.plot(Mag,rt/Rd)
-        plt.scatter(m,rPE_)
+    if True:
+        plt.figure(1)
+        plt.plot(Mag,np.log10(vt_0))
+        plt.scatter(m,np.log10(V0_))
+
+        plt.figure(2)
+        plt.plot(Mag,np.log10(rt/Ropt))
+        plt.scatter(m,np.log10(rPE_))
         plt.show()
 
-    # Optical Radius, also Slope from NIHAO 17 
-    R_opt = (3.31+err(0.01))*Rd
+
+    # Set slope from NIHAO 17 
     slope_sparc = 0.123 - 0.137*(np.log10(mstar)-9.471) + err(0.19)
 
     # Find Vrot, then Alpha, then check again to make sure Vrot is consistent
     for i in range(0,6):
+
         # Make parameters for all Magnitudes
         Mag = np.arange(-25,0.,0.001)
         vt_0=func(Mag,*V0)
-        rt=Rd*func3(Mag,*rPE)
+        rt=Ropt*func3(Mag,*rPE)
        
         # Outer edge, and half of it for the slope
         x2 = RHI * 2.
         x1 = RHI
-        # Calculate rotation velocities at R_opt for all vt_0, rt
-        vt = vt_0 * ( 1. - np.exp(-R_opt/rt) ) * ( 1. + a * R_opt/rt )
+        # Calculate rotation velocities at Ropt for all vt_0, rt
+        vt = vt_0 * ( 1. - np.exp(-Ropt/rt) ) * ( 1. + a * Ropt/rt )
 
         # Best guess for Magnitude based on vrot with other params
         # Finds index of vt that most closely matches vrot and 
         # that matches the Magnitude
         ind = np.argmin(abs(vt-vrot))
-        Mag = round(Mag[ind],2)
-        if len(np.array([a])) != 1.: a = a[ind]
-        print('1: Target Vrot:',np.round(vrot,2),'Current Vrot:',np.round(vt[ind],2),"Mag:",Mag,'Alpha',np.round(a,6),'Max Possible',np.max(vt))
-
-        vt_0_plt = vt_0
-        rt_plt   = rt
-
-        vt = vt[ind]
-        rt = rt[ind]
+        Mag  = Mag[ind]
+        vt   = vt[ind]
+        rt   = rt[ind]
         vt_0 = vt_0[ind]
-        
+       
+        # Consider a range of values of alpha
         a = np.arange(-0.04,0.4,0.0001)
-        a_plt = a
-   
         slope1 = ((1.-np.exp(-x2/rt))*(1.+a*x2/rt))
         slope2 = ((1.-np.exp(-x1/rt))*(1.+a*x1/rt))
+        # Only want values where logv is defined (v>0)
         slope1_log = np.log10(slope1[(slope1 > 0) & (slope2 > 0)])
         slope2_log = np.log10(slope2[(slope1 > 0) & (slope2 > 0)])
         a = a[(slope1 > 0) & (slope2 > 0)]
+
+        # Calculate delta logv / delta log r
+        # Find value of a that gives value closest to NIHAO
         slope = (slope1_log-slope2_log) / (np.log10(x2)-np.log10(x1))
-        print(len(a),len(slope))
         a = a[np.argmin(abs(slope - slope_sparc))]
 
-   #     slope1 = np.zeros_like(a)*9E9
-   #     slope2 = np.zeros_like(a)*9E9
-   #     slope1_log = np.zeros_like(a)*9E9
-   #     slope2_log = np.zeros_like(a)*9E9
-   #     slope  = np.zeros_like(a)*9E9
-   #
-   #     for ind, value in enumerate(a):
-   #         slope1[ind] = ((1.-np.exp(-x2/rt))*(1.+a[ind]*x2/rt))
-   #         slope2[ind] = ((1.-np.exp(-x1/rt))*(1.+a[ind]*x1/rt))
-   #         if slope1[ind] > 0 and slope2[ind] >0:
-   #             slope1_log[ind] = np.log10(slope1[ind])
-   #             slope2_log[ind] = np.log10(slope2[ind])
-   #             slope[ind] = (slope1_log[ind]-slope2_log[ind]) / (np.log10(x2)-np.log10(x1))
-    
-   #     a = a[np.argmin(abs(slope - slope_sparc))]
-    
-        vt = vt_0 * ( 1. - np.exp(-R_opt/rt) ) * ( 1. + a * R_opt/rt )
-        vtest = vt_0_plt * ( 1. - np.exp(-R_opt/rt_plt) ) * ( 1. + a * R_opt/rt_plt )
-        #print(len(rt),len(vt_0))
-        #print('HERE: adjusted vt',vt,'adjusted a',a,'best slope',slope[np.argmin(abs(slope[np.isfinite(slope)] - slope_sparc))],'real slope',slope_sparc)
-        print('2: Target Vrot:',np.round(vrot,2),'Current Vrot:',np.float(np.round(vt,2)),"Mag:",Mag,'Alpha',np.round(a,6))
-    if True:
-            Mag_plt = np.arange(-25,0.,0.001)
-            plt.plot(Mag_plt[np.argmax(vtest):],vtest[np.argmax(vtest):])
-            plt.plot(Mag_plt[np.argmax(vtest):],vt_0_plt[np.argmax(vtest):])
-            plt.show()
-    if False:
-            plt.plot(a_plt[(slope1>0) & (slope2>0)],slope)
-            plt.xlabel('a: polyex outer slope')
-            plt.ylabel('$\Delta$log(v)/$\Delta$log(r)')
-            plt.show()
+    vt_1  = vt_0*(1.-np.exp(-x2/rt))*(1.+a*x2/rt)
+    vt_2  = vt_0*(1.-np.exp(-x1/rt))*(1.+a*x1/rt)
+    slope = (np.log10(vt_1)-np.log10(vt_2))/(np.log10(x2)-np.log10(x1))
+
     return Mag,a,slope
 
 def sbr_calc(radi,RHI,x,dx,vt,Rs):
+    # Return the surface brightness profile using the formula from PK
     sbr = np.zeros_like(radi)
+    # Go through every point in the rotation curve
+    # (For speed improvement maybe just make this array operations)
     for j, r in enumerate(radi):
         sig1 = np.exp(-((r-0.4*RHI)/(np.sqrt(2)*(x+dx)*RHI))**2.)
         sig2 = (np.sqrt(vt/120.)-1.)*np.exp(-r/Rs)
         sbr[j] = sig1-sig2
         if sbr[j] < 0:
             sbr[j] = 0
-    R_HI= np.argmin(abs(radi**2. - RHI**2.))
+    # Find the closest point to the 1 Msun/pc radius
+    R_HI= np.argmin(abs(radi - RHI))
+
+    # Return normalized surface brightness profile
     sbr = sbr/sbr[R_HI]
     return sbr
 
 
 def make_sbr(radi,Rs,DHI,vt,mass):
+    # Make the surface brightness profile
     RHI=DHI/2.
     x=0.36
+
+    # consider a range of x+dx to get closest match to HI mass
     delta = np.arange(-0.045,-0.03,0.001)
     Mass_guess = np.zeros_like(delta)
-
     for i, dx in enumerate(delta):
         sbr = sbr_calc(radi,RHI,x,dx,vt,Rs)
         Mass_guess[i] = (integrate.simps(sbr*2*np.pi*radi,radi)*1000.**2.)
-
-    Mj = np.argmin(abs(Mass_guess**2.- mass**2.))
+    Mj = np.argmin(abs(Mass_guess- mass))
     dx = delta[Mj]
 
+    # When closest one is found, calculate it and return it
     sbr = sbr_calc(radi,RHI,x,dx,vt,Rs)
     Mass_guess = (integrate.simps(sbr*2*np.pi*radi,radi)*1000.**2.)
-    #print('Mass %:',100*Mass_guess/mass)
+    print(np.log10(mass),np.log10(Mass_guess))
     
     return sbr
 
@@ -235,7 +226,7 @@ def expdisk(v,slope,const,scatr):
     const = const[0] + err(const[1]) + err(scatr)
     return 10.**(const + slope * np.log10(v))
 
-def setup_relations(mass,beams,thicc):
+def setup_relations(mass,beams,ring_thickness):
     ######################################################
     MHI = np.round(10.**(np.arange(6.,11.1,.1)),1)
     mass=10.**float(mass)
@@ -295,42 +286,36 @@ def setup_relations(mass,beams,thicc):
     slope = np.array([0.56,0.04])
     const = np.array([-0.36,0.08])
     scatr = np.array([0.16])
-    Rd = expdisk(vflat,slope,const,scatr)
-    # Rd I-band
+    Ropt = expdisk(vflat,slope,const,scatr)
+    # Ropt I-band
 
     #####################################################
     # Calculating Magnitude from vmax
     # Catinella et al 2005
     # https://arxiv.org/abs/astro-ph/0512051
-    Mag,alpha,rc_slope = Magcalc(vflat,Rd,DHI/2.,Mstar)
+    Mag,alpha,slope = Magcalc(vflat,Ropt,DHI/2.,Mstar)
     # I-band mag
-
     #####################################################
-    # Velocity dispersion to range from 8km/s
-    # for most massive, to 20km/s for least
-    # massive?
+    # Compute radial sampling cadence
+    dist  = DHI / (4.*beams * (np.pi/162000.))
+    dist  = dist * u.kpc
+    print(dist)
+    delta = ((ring_thickness*u.arcsec).to_value(u.rad)*(dist))* u.kpc
+    print(delta)
+    # Compute radi, rotation curve, surface brightness profile
+    radi     = np.arange(0.,DHI,delta/u.kpc)
+    vrot,rPE = make_vrot(radi,Mag,Ropt,alpha)
+    # Convert SBR to mJy
+    sbr      = (1./dist)*(1/0.236)*make_sbr(radi,Rs,DHI,vflat,mass)
+    #####################################################
+    # Velocity dispersion 8km/s
     # Constant for now.
+    # Use it to calculate disk thickness based on
+    # Puche et al 1992
+    # http://adsabs.harvard.edu/abs/1992AJ....103.1841P
     Vdisp = 8.
-
-    dist = DHI / (4.*beams * (np.pi/162000.))
-
-    dist = dist * u.kpc
-    dist = dist.to_value(u.kpc)
-
-    #print('distance [kpc]', round(dist,2))
-    delta = ((thicc*u.arcsec).to_value(u.rad)*(dist))* u.kpc
-    #print('ringsize',np.round(delta/(1*u.kpc),4))
-    #print('rings',np.round(DHI*u.kpc /delta,2))
-    radi = np.arange(0.,DHI,delta/u.kpc)
-
-    vrot,rPE = make_vrot(radi,Mag,Rd,vflat,DHI/2.,Mstar)
-    sbr  = (1./dist)*(1/0.236)*make_sbr(radi,Rs,DHI,vflat,mass)
-    #print('Sigma0',round(np.max(sbr),6))
     z    = make_z(radi,vrot,Vdisp)
-
-    mid = int(len(radi)/2)-1
-    end = len(radi)-1
-    slope = (np.log10(vrot[end])-np.log10(vrot[mid]))/(np.log10(radi[end])-np.log10(radi[mid]))
+    #####################################################
    
     make_plots = False
     if (make_plots):
@@ -351,4 +336,4 @@ def setup_relations(mass,beams,thicc):
         plt.savefig('VROT.png')
         plt.close(2)
     rothead(MHI,Mag,Vdisp,Mbar,Mstar,DHI,vflat,Rs,dist)
-    return radi, sbr, vrot, Vdisp, z, MHI, DHI, Mag, dist, alpha,vflat,Mstar,slope,Rd*3.31,rPE
+    return radi, sbr, vrot, Vdisp, z, MHI, DHI, Mag, dist, alpha,vflat,Mstar,slope,Ropt,rPE
