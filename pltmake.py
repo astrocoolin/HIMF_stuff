d9d9212d05907f75a485f921317912a36efbf272
Colin Lewis <25259436+astrocoolin@users.noreply.github.com>
2018-12-10 12:57:29 -0500

    More plot changes

--- pltmake.py
+++ pltmake.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from astropy.io import ascii
 from pathlib import Path
@@ -28,8 +30,16 @@ for gal in (1,2,3,4,5,):
     rPE = float(lines[16][18:])
     DHI = float(lines[9][19:])
     Distance = float(lines[8][19:])
-    
+    infile.close()
+
+    #Finding r where sbr drops < 0.5
+    r_prof = ascii.read(fname[0]+"/RC.dat")['col1']
+    sbr = ascii.read(fname[0]+"/RC.dat")['col3'] 
+    conv=6.0574E5*1.823E18*(2.*np.pi/np.log(256.))
+    sbr = sbr/1.24756e+20*(conv)
+    R_HI5 = r_prof[np.argmin(abs(sbr - 0.5))]
     DHI = ((DHI/2.) / Distance)*3600.*180./np.pi
+    #print(DHI,R_HI5)
     
     VROT = np.zeros([50,50])*np.nan
     RADI = np.zeros([50,50])*np.nan
@@ -55,6 +65,7 @@ for gal in (1,2,3,4,5,):
         else:
             print(num,'Doesnt Exist')
 
+    print(np.unique(RADI))
     RADI_med = np.unique(RADI)
     RADI_med = RADI_med[np.isfinite(RADI_med)]
 
@@ -66,6 +77,7 @@ for gal in (1,2,3,4,5,):
     
     RADI_2 = np.arange(0,np.max(RADI_med)+0.1,0.1)
     polyex = v0 * (1. - np.exp(-RADI_2/rPE)) * (1. + RADI_2 * a /rPE )
+    polyex_discrete = v0 * (1. - np.exp(-RADI_med/rPE)) * (1. + RADI_med * a /rPE )
 
     for i,radius in enumerate(RADI_med):
 
@@ -77,32 +89,57 @@ for gal in (1,2,3,4,5,):
 
         VROT_temp = VROT[(RADI==radius) & (np.isfinite(VROT))]
 
-        if VROT_temp.size > 4  or (i < instr_len):
-            top,bot = np.percentile(VROT_temp, [75 ,25],axis=0)
+        #if VROT_temp.size > 4  or (i < instr_len):
+        top,bot = np.percentile(VROT_temp, [75 ,25],axis=0)
 
-            iqr = np.append(iqr,top-bot)
-            devarray = np.append(devarray,np.std(VROT_temp))
+        iqr = np.append(iqr,top-bot)
+        devarray = np.append(devarray,np.std(VROT_temp))
+    Bias = np.array([]) 
+    for i in range(0,len(RADI)):
+        RADI_temp = RADI[i][np.isfinite(RADI[i])]
+        C = (1./50.) / len(RADI_temp[RADI_temp < R_HI5])
+        polyex_discrete = v0 * (1. - np.exp(-RADI_temp/rPE)) * (1. + RADI_temp * a /rPE )
+        B = np.array([])
+        sig = np.array([])
+        for j,r in enumerate(RADI_temp):
+            if r < R_HI5:
+                #print(VROT[i][j],polyex_discrete[j],iqr[RADI_med==r],r)
+                B = np.append(B,VROT[i][j] - polyex_discrete[j])
+                sig = np.append(sig,iqr[RADI_med==r])
+        B = B[np.isfinite(B)]
+        #print(np.sum((C*B/sig)[np.isfinite(C*B/sig)]))
+        Bias = np.append(Bias,np.sum((C*B/sig)[np.isfinite(C*B/sig)]))
+    Total_Bias = np.sum(Bias)
+    
+    polyex_discrete = v0 * (1. - np.exp(-RADI_med/rPE)) * (1. + RADI_med * a /rPE )
+    residual = VROT_med-polyex_discrete
+    residual_int = np.zeros_like(residual)*np.nan
+    error_int = np.zeros_like(residual)*np.nan
+    for i, r in enumerate(residual):
+            residual_int[i] = np.sum(residual[0:i+1])
+            error_int[i] = np.sum(iqr[0:i+1])
 
     delta_1 = np.array([])
     delta_2 = np.array([])
-
+    iqr_within = np.array([])
+    dev_within = np.array([])
+    norm = 0
     for i,r in enumerate(RADI_med):
-        delta_1 = np.append(delta_1,abs(VROT_med[i] -polyex[RADI_2 ==r]))
-        delta_2 = np.append(delta_2,abs(VROT_mean[i]-polyex[RADI_2 ==r]))
+        if r <= R_HI5:
+            norm = norm+1
+            delta_1 = np.append(delta_1,abs(VROT_med[i] -polyex[RADI_2 ==r]))
+            delta_2 = np.append(delta_2,abs(VROT_mean[i]-polyex[RADI_2 ==r]))
+            iqr_within = np.append(iqr_within,iqr[i])
+            dev_within = np.append(dev_within,devarray[i])
 
     RADI_med = RADI_med[np.isfinite(VROT_med)]
     VROT_med = VROT_med[np.isfinite(VROT_med)]
-
-    norm = len(iqr[np.nonzero(iqr)])
     chimed=np.array([])
     chi2=np.array([])
-    
-    chimed = np.sum(delta_1[np.nonzero(iqr)]**2./iqr[np.nonzero(iqr)])/norm
-    chi2   = np.sum(delta_2[np.nonzero(iqr)]**2./devarray[np.nonzero(iqr)])/norm
+    chimed = np.sum(delta_1[np.nonzero(iqr_within)]**2./iqr_within[np.nonzero(iqr_within)])/norm
+    chi2   = np.sum(delta_2[np.nonzero(iqr_within)]**2./dev_within[np.nonzero(iqr_within)])/norm
    
     fig=plt.figure(figsize=(12,8))
-    #gs = gridspec.GridSpec(2, 1,height_ratios=[3,1])
-
     gs0 = gridspec.GridSpec(1, 2)
     gs00 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[0])
 
@@ -111,9 +148,6 @@ for gal in (1,2,3,4,5,):
     ax3 = plt.Subplot(fig, gs00[2, :])
     fig.add_subplot(ax3)
 
-
-    #ax1 = plt.subplot(gs[0])
-    #ax3 = plt.subplot(gs[1])
     
     ax1.plot(RADI_med,VROT_med, lw = 2.5, label = 'Recovered Curve')
     ax1.fill_between(RADI_med, VROT_med-iqr, VROT_med+iqr,alpha=0.4)
@@ -122,13 +156,11 @@ for gal in (1,2,3,4,5,):
     ax1.set_xlabel('Radius (arcsec)')
 
     ax2 = ax1.twiny()
-
-    #print(Distance*DHI/(206265*2.))
     plt.title(namestr+" \nChi^2 = "+str(chi2),y=1.10)
     ax2.set_xlim(0,Distance*DHI/(206265*2.))
     ax2.set_xlabel('\n Radius (kpc)')
 
-    ax1.axvline(DHI)
+    ax1.axvline(R_HI5)
     ax1.legend()
     N=(len(np.unique(RADI[np.isfinite(RADI)])))
     ax3.hist(RADI[np.isfinite(RADI)],N,facecolor='white',edgecolor='#1f77b4')
@@ -136,66 +168,35 @@ for gal in (1,2,3,4,5,):
     ax3.set_ylabel('Number of Points \n per Radial Bin')
     ax3.set_xlabel('Radius (arcsec)')
 
-    from astropy.io import fits
-    basename = "ba_"+str(ba)+".0.mass_"+str(mass)+".inc_60.0.SN_8.0"
-    
-    pvin  = basename+".gal_"+str(gal)+".noise1/PV-Diagrams/Cube_"+basename+"_preprocessed_small_0_xv.fits"
-    existcheck = Path(pvin)
-    if not existcheck.is_file():
-        pvin = basename+".gal_"+str(gal)+".noise1/PV-Diagrams/Cube_"+basename+"_small_0_xv.fits"
-
-    hdulist = fits.open(pvin)
-    pvin_cube = hdulist[0].data
-        
-    pvout = basename+".gal_"+str(gal)+".noise1/PV-Diagrams/Finalmodel_xv.fits"
-    hdulist = fits.open(pvout)
-    pvout_cube = hdulist[0].data
-    
-    gs01 = gridspec.GridSpecFromSubplotSpec(2, 6, subplot_spec=gs0[1])
-    gx1 = plt.Subplot(fig, gs01[0:1, 0:3])
+    gs01 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[1])
+    gx1 = plt.Subplot(fig, gs01[0:2, :])
     fig.add_subplot(gx1)
-
-    gx2 = plt.Subplot(fig, gs01[0:1, 3:6])
+    gx2 = plt.Subplot(fig, gs01[2, :])
     fig.add_subplot(gx2)
+ 
+    gx1.plot(RADI_med,residual)
+    gx1.fill_between(RADI_med, residual-iqr, residual+iqr,alpha=0.4)
+    gx1.fill_between(RADI_med, residual, 0 ,where= residual>0,alpha=0.4,color='green',interpolate=True)
+    gx1.fill_between(RADI_med, residual, 0 ,where= residual<0,alpha=0.4,color='red',interpolate=True)
+    gx1.axvline(R_HI5)
 
-    gx3 = plt.Subplot(fig, gs01[1:2, 0:3])
-    fig.add_subplot(gx3)
     
-    gx4 = plt.Subplot(fig, gs01[1:2, 4:5])
-    fig.add_subplot(gx4)
-    
-    wdiff = np.shape(pvin_cube)[1] - np.shape(pvout_cube)[1]
-    if wdiff > 0:
-        start = int(wdiff/2)
-        pvin_cube=pvin_cube[:,start:-start]
-    elif wdiff < 0:
-        start = int(abs(wdiff/2))
-        pvout_cube=pvout_cube[:,start:-start]
+    #gx1.set_title('Residual curve (sums within DHI)\n Residual Sum $\Sigma r_i$='+str(residual_int[np.argmin(abs(RADI_med - R_HI5))])+'\nUncertainty Sum $\Sigma \delta_i$='+str(error_int[np.argmin(abs(RADI_med - R_HI5))]) )
+    gx1.set_title('"Bias"='+str(Total_Bias))
 
-    top = np.max(np.array([pvout_cube,pvin_cube]))
-    bot = np.min(np.array([pvout_cube,pvin_cube]))
-    
-    gx1.imshow(pvin_cube,vmin=bot,vmax=top,cmap='gray')
-    gx1.axis('off')
 
-    gx2.imshow(pvout_cube,vmin=bot,vmax=top,cmap='gray')
-    gx2.axis('off')
+    #print(RADI_med,R_HI5)
+    #print(residual_int)
 
-    gx3.imshow(abs(pvin_cube-pvout_cube),vmin=bot,vmax=top,cmap='gray')
-    gx3.axis('off')
+    gx2.plot(RADI_med,residual_int)
+    gx2.axvline(R_HI5)
 
-    import matplotlib as mpl
-    cmap = mpl.cm.gray
-    norm = mpl.colors.Normalize(vmin=bot, vmax=top)
-    cb1 = mpl.colorbar.ColorbarBase(gx4, cmap=cmap,
-                                norm=norm,
-                                orientation='vertical')
+    gx2.fill_between(RADI_med,residual_int, 0 \
+            ,where= residual_int>0,alpha=0.4,color='green',interpolate=True)
+    gx2.fill_between(RADI_med,residual_int, 0 \
+            ,where= residual_int<0,alpha=0.4,color='red',interpolate=True)
 
-    gx1.set_title('Sum='+str(np.sum(pvin_cube))+"\nMax="+str(np.max(pvin_cube))+"\nMin="+str(np.min(pvin_cube)))
-    gx2.set_title('Sum='+str(np.sum(pvout_cube))+"\nMax="+str(np.max(pvout_cube))+"\nMin="+str(np.min(pvout_cube)))
-    gx3.set_title('Sum='+str(np.sum(pvin_cube-pvout_cube))+"\nMax="+str(np.max(pvin_cube-pvout_cube))+"\nMin="+str(np.min(pvin_cube-pvout_cube)))
-    gx4.set_title('Colorbar')
-   
     plt.tight_layout()
+    plt.savefig(namestr+'plot.png',bbox_inches='tight')
     plt.show()
 
