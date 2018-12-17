d550953939b1ce486927a1f1561428df7a22c423
Colin Lewis <25259436+astrocoolin@users.noreply.github.com>
2018-12-17 11:27:24 -0500

    updating with early data analysis stuff

--- pltmake.py
+++ pltmake.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib as mpl
-mpl.use('Agg')
+#mpl.use('Agg')
 import matplotlib.pyplot as plt
 from astropy.io import ascii
 from pathlib import Path
 
-import matplotlib.gridspec as gridspec
+plt1 = False
 
+import matplotlib.gridspec as gridspec
+alldelta = np.array([])
+alldelta_r = np.array([])
+#xy = np.array([])
+xy = []
 
 for gal in (1,2,3,4,5,):
  for mass in (6.5,6.0,7.5,8.0,8.5,9.0,):
@@ -127,76 +132,96 @@ for gal in (1,2,3,4,5,):
     for i,r in enumerate(RADI_med):
         if r <= R_HI5:
             norm = norm+1
-            delta_1 = np.append(delta_1,abs(VROT_med[i] -polyex[RADI_2 ==r]))
-            delta_2 = np.append(delta_2,abs(VROT_mean[i]-polyex[RADI_2 ==r]))
+            delta_1 = np.append(delta_1,VROT_med[i] -polyex[RADI_2 ==r])
+            delta_2 = np.append(delta_2,VROT_mean[i]-polyex[RADI_2 ==r])
             iqr_within = np.append(iqr_within,iqr[i])
             dev_within = np.append(dev_within,devarray[i])
 
     RADI_med = RADI_med[np.isfinite(VROT_med)]
     VROT_med = VROT_med[np.isfinite(VROT_med)]
+    
+    alldelta = np.append(alldelta,delta_1)
+    alldelta_r = np.append(alldelta_r,RADI_med[RADI_med <= R_HI5]/R_HI5)
     chimed=np.array([])
     chi2=np.array([])
-    chimed = np.sum(delta_1[np.nonzero(iqr_within)]**2./iqr_within[np.nonzero(iqr_within)])/norm
-    chi2   = np.sum(delta_2[np.nonzero(iqr_within)]**2./dev_within[np.nonzero(iqr_within)])/norm
-   
-    fig=plt.figure(figsize=(12,8))
-    gs0 = gridspec.GridSpec(1, 2)
-    gs00 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[0])
+    chimed = np.sum(delta_1[np.nonzero(iqr_within)]**2./iqr_within[np.nonzero(iqr_within)]**2.)/norm
+    chi2   = np.sum(delta_2[np.nonzero(iqr_within)]**2./dev_within[np.nonzero(iqr_within)]**2.)/norm
+    xy.append([[chimed,Total_Bias]])
 
-    ax1 = plt.Subplot(fig, gs00[0:2, :])
-    fig.add_subplot(ax1)
-    ax3 = plt.Subplot(fig, gs00[2, :])
-    fig.add_subplot(ax3)
+    if plt1: 
+        fig=plt.figure(figsize=(12,8))
+        gs0 = gridspec.GridSpec(1, 2)
+        gs00 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[0])
 
-    
-    ax1.plot(RADI_med,VROT_med, lw = 2.5, label = 'Recovered Curve')
-    ax1.fill_between(RADI_med, VROT_med-iqr, VROT_med+iqr,alpha=0.4)
-    ax1.plot(RADI_2,polyex, lw = 2.5 , label = 'Input Curve')
-    ax1.set_ylabel('Vrot (km/s)')
-    ax1.set_xlabel('Radius (arcsec)')
+        ax1 = plt.Subplot(fig, gs00[0:2, :])
+        fig.add_subplot(ax1)
+        ax3 = plt.Subplot(fig, gs00[2, :])
+        fig.add_subplot(ax3)
 
-    ax2 = ax1.twiny()
-    plt.title(namestr+" \nChi^2 = "+str(chi2),y=1.10)
-    ax2.set_xlim(0,Distance*DHI/(206265*2.))
-    ax2.set_xlabel('\n Radius (kpc)')
+        
+        ax1.plot(RADI_med,VROT_med, lw = 2.5, label = 'Recovered Curve')
+        ax1.fill_between(RADI_med, VROT_med-iqr, VROT_med+iqr,alpha=0.4)
+        ax1.plot(RADI_2,polyex, lw = 2.5 , label = 'Input Curve')
+        ax1.set_ylabel('Vrot (km/s)')
+        ax1.set_xlabel('Radius (arcsec)')
 
-    ax1.axvline(R_HI5)
-    ax1.legend()
-    N=(len(np.unique(RADI[np.isfinite(RADI)])))
-    ax3.hist(RADI[np.isfinite(RADI)],N,facecolor='white',edgecolor='#1f77b4')
-    ax3.hist(RADI[np.isfinite(RADI)],N,facecolor='#1f77b4',edgecolor='#1f77b4',alpha=0.1)
-    ax3.set_ylabel('Number of Points \n per Radial Bin')
-    ax3.set_xlabel('Radius (arcsec)')
+        ax2 = ax1.twiny()
+        plt.title(namestr+" \nChi^2 = "+str(chi2),y=1.10)
+        ax2.set_xlim(0,Distance*DHI/(206265*2.))
+        ax2.set_xlabel('\n Radius (kpc)')
 
-    gs01 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[1])
-    gx1 = plt.Subplot(fig, gs01[0:2, :])
-    fig.add_subplot(gx1)
-    gx2 = plt.Subplot(fig, gs01[2, :])
-    fig.add_subplot(gx2)
+        ax1.axvline(R_HI5)
+        ax1.legend()
+        N=(len(np.unique(RADI[np.isfinite(RADI)])))
+        ax3.hist(RADI[np.isfinite(RADI)],N,facecolor='white',edgecolor='#1f77b4')
+        ax3.hist(RADI[np.isfinite(RADI)],N,facecolor='#1f77b4',edgecolor='#1f77b4',alpha=0.1)
+        ax3.set_ylabel('Number of Points \n per Radial Bin')
+        ax3.set_xlabel('Radius (arcsec)')
+
+        gs01 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[1])
+        gx1 = plt.Subplot(fig, gs01[0:2, :])
+        fig.add_subplot(gx1)
+        gx2 = plt.Subplot(fig, gs01[2, :])
+        fig.add_subplot(gx2)
  
-    gx1.plot(RADI_med,residual)
-    gx1.fill_between(RADI_med, residual-iqr, residual+iqr,alpha=0.4)
-    gx1.fill_between(RADI_med, residual, 0 ,where= residual>0,alpha=0.4,color='green',interpolate=True)
-    gx1.fill_between(RADI_med, residual, 0 ,where= residual<0,alpha=0.4,color='red',interpolate=True)
-    gx1.axvline(R_HI5)
+        gx1.plot(RADI_med,residual)
+        gx1.fill_between(RADI_med, residual-iqr, residual+iqr,alpha=0.4)
+        gx1.fill_between(RADI_med, residual, 0 ,where= residual>0,alpha=0.4,color='green',interpolate=True)
+        gx1.fill_between(RADI_med, residual, 0 ,where= residual<0,alpha=0.4,color='red',interpolate=True)
+        gx1.axvline(R_HI5)
 
-    
-    #gx1.set_title('Residual curve (sums within DHI)\n Residual Sum $\Sigma r_i$='+str(residual_int[np.argmin(abs(RADI_med - R_HI5))])+'\nUncertainty Sum $\Sigma \delta_i$='+str(error_int[np.argmin(abs(RADI_med - R_HI5))]) )
-    gx1.set_title('"Bias"='+str(Total_Bias))
+        gx1.set_title('"Bias"='+str(Total_Bias))
 
+        gx2.plot(RADI_med,residual_int)
+        gx2.axvline(R_HI5)
 
-    #print(RADI_med,R_HI5)
-    #print(residual_int)
+        gx2.fill_between(RADI_med,residual_int, 0 \
+                ,where= residual_int>0,alpha=0.4,color='green',interpolate=True)
+        gx2.fill_between(RADI_med,residual_int, 0 \
+                ,where= residual_int<0,alpha=0.4,color='red',interpolate=True)
 
-    gx2.plot(RADI_med,residual_int)
-    gx2.axvline(R_HI5)
+        plt.tight_layout()
+        plt.savefig(namestr+'plot.png',bbox_inches='tight')
+        plt.close()
 
-    gx2.fill_between(RADI_med,residual_int, 0 \
-            ,where= residual_int>0,alpha=0.4,color='green',interpolate=True)
-    gx2.fill_between(RADI_med,residual_int, 0 \
-            ,where= residual_int<0,alpha=0.4,color='red',interpolate=True)
+fig=plt.figure(figsize=(12,8))
+xy = np.array(xy)
+for i in range(0,len(xy)):
+    if (xy[i,0][0]) < 0 or (xy[i,0][1]) < 0:
+        plt.scatter((xy[i,0][0]),(xy[i,0][1]),color='red')
+    else:
+        plt.scatter((xy[i,0][0]),(xy[i,0][1]),color='blue')
+plt.xscale('symlog')
+plt.yscale('symlog')
+plt.ylabel('"Bias"')
+plt.xlabel('Chi2')
+plt.savefig('test1.png',bbox_inches='tight')
+plt.close()
+fig=plt.figure(figsize=(12,8))
+plt.xlabel('R/R_HI')
+plt.ylabel('Vrot - Polyex')
+plt.scatter(alldelta_r[alldelta<0],alldelta[alldelta<0])
+plt.scatter(alldelta_r[alldelta>0],alldelta[alldelta>0])
+plt.savefig('test2.png',bbox_inches='tight')
 
-    plt.tight_layout()
-    plt.savefig(namestr+'plot.png',bbox_inches='tight')
-    plt.show()
-
+#plt.show()
