ae9084c0124e5b4e0b036a19fdbddba6df900eca
Colin Lewis <25259436+astrocoolin@users.noreply.github.com>
2019-01-31 13:42:05 -0500

    going to make multiplot plots

--- pltmake.py
+++ pltmake.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from scipy import optimize
 
 # am I plotting the data individually
-plt1 = True
+plt1 = False
 
 # array of all points 
 alldelta = np.array([])
@@ -18,6 +18,7 @@ alldelta_r = np.array([])
 
 # array of statistical measures
 xy = []
+xy_f = []
         
 def plyex(v0,rPE,a,R):
     return v0 * (1. - np.exp(-R/rPE)) * (1. + R * a /rPE )
@@ -34,12 +35,12 @@ def rect(axob,xmin,ymin,dx,dy):
     coll=PatchCollection(rob, zorder=1,alpha=0.45,color='C0')
     axob.add_collection(coll)
 
-    
 # all galaxies
-for mass in (8.3,):
+for mass in (7.5, 8.3, 9.2, 10.5):
   for ba in (4,5,6,8,):
       for inc in (10,35,65,80,):
-        namestr = "ba_"+str(ba)+".0.mass_"+str(mass)+".inc_"+str(inc)+".0.SN_8.0.noise"
+        namestr = str(mass)+"/ba_"+str(ba)+".0.mass_"+str(mass)+".inc_"+str(inc)+".0.SN_8.0.noise"
+        saveloc = "ba_"+str(ba)+".0.mass_"+str(mass)+".inc_"+str(inc)+".0.SN_8.0.noise"
         num = np.arange(1,51)
        
         #name all the variables
@@ -89,7 +90,6 @@ for mass in (8.3,):
         VROT = np.zeros([50,50])*np.nan
         RADI = np.zeros([50,50])*np.nan
         INCL = np.zeros([50,50])*np.nan
-        deltaI = 0
         
         instr = 0
         instr_len = 0
@@ -111,18 +111,24 @@ for mass in (8.3,):
                             instr_len = len(instr)
                     if   'INCL' in line and "+" in line and "ERR" not in line and "_2" not in line:
                         instr = np.array(np.float32(line.split()[1:]))
-                        #deltaI = max((np.max(instr[np.isfinite(instr)]) - np.min(instr[np.isfinite(instr)])),deltaI)
-                        INCL[num,0:len(instr[:-1])]=instr[:-1]
-
+                        #INCL[num,0:len(instr[:-1])]=instr[:-1]
+                        INCL[num,0:len(instr)]=instr
         
-        print(deltaI,'deltaI',valu) 
+
+
         # smooth radii for analytical ployex. This is an array
+        if len(RADI[np.isfinite(RADI)]) == 0: continue
+        #print(RADI)
+        #print(np.max(RADI[np.isfinite(RADI)]),saveloc)
         RADI_2 = np.arange(0,np.max(RADI[np.isfinite(RADI)])+0.1,0.1)
         polyex = v0 * (1. - np.exp(-RADI_2/rPE)) * (1. + RADI_2 * a /rPE )
 
         # This does the data for the plot of all rotation curves
         V = VROT[np.isfinite(RADI)]
-        R = RADI[np.isfinite(RADI)] 
+        R = RADI[np.isfinite(RADI)]
+        I = INCL[np.isfinite(RADI)]
+        VROT_f = VROT * np.sin(INCL*np.pi/180.)/np.sin(inc*np.pi/180.)
+        V2 = V*np.sin(I*np.pi/180.)/np.sin(inc*np.pi/180.)
         
         # FOR STATISTICS
         # unique radii, finite values only
@@ -188,6 +194,10 @@ for mass in (8.3,):
         # FOR BIAS AND CHI2
         Bias = np.array([])
         Chi2 = np.array([])
+
+        Bias_f = np.array([])
+        Chi2_f = np.array([])
+
         for i in range(0,len(RADI)):
             RADI_temp = RADI[i][np.isfinite(RADI[i])]
             if len(RADI_temp[RADI_temp < R_HI5]) <= 0: continue
@@ -196,56 +206,95 @@ for mass in (8.3,):
             B = np.array([])
             sig = np.array([])
 
+            B_f = np.array([])
+            sig_f = np.array([])
+
             for j,r in enumerate(RADI_temp):
                 if r < R_HI5:
                     B = np.append(B,VROT[i][j] - polyex_discrete[j])
+                    B_f = np.append(B_f,VROT_f[i][j] - polyex_discrete[j])
+
                     top,bot = np.percentile(VROT[(RADI==r) 
                         & np.isfinite(VROT)], [75 ,25],axis=0)
                     sig = np.append(sig,top-bot)
+
+                    top,bot = np.percentile(VROT_f[(RADI==r) 
+                        & np.isfinite(VROT_f)], [75 ,25],axis=0)
+                    sig_f = np.append(sig_f,top-bot)
+
+            sig_f = sig_f[np.isfinite(B_f)]
             sig = sig[np.isfinite(B)]
+
             B = B[np.isfinite(B)]
+            B_f = B_f[np.isfinite(B_f)]
             
             Bias = np.append(Bias,np.sum((C*B/sig)[np.isfinite(C*B/sig)]))
             Chi2 = np.append(Chi2,np.sum((C*B**2./sig**2.)[np.isfinite(C*B**2./sig**2.)]))
+            
+            Bias_f = np.append(Bias_f,np.sum((C*B_f/sig_f)[np.isfinite(C*B_f/sig_f)]))
+            Chi2_f = np.append(Chi2_f,np.sum((C*B_f**2./sig_f**2.)[np.isfinite(C*B_f**2./sig_f**2.)]))
 
         Total_Bias = np.sum(Bias)
         Total_Chi = np.sum(Chi2)
-       
-        xy.append([[Total_Chi,Total_Bias]])
-
-        fig = plt.figure(figsize=(12,4))
-        ax1=fig.add_subplot(1, 2, 1)
-        ax2=fig.add_subplot(1, 2, 2)
-
-        ax1.scatter(RADI,VROT, lw = 2.5, label = 'Recovered Curve',
-                marker='x',alpha=0.25,color='C0',zorder=2)
-        ax2.scatter(res_radi,residuals,color='C0',alpha=0.25,zorder=2)
-        textstr = '\n'.join((
-            r'$\mathrm{Max (i)}=%.2f$' % (np.max(INCL[np.isfinite(INCL)]),),
-            r'$\mathrm{Min (i)}=%.2f$' % (np.min(INCL[np.isfinite(INCL)]),),
-            r'$\mathrm{Med (i)}=%.2f$' % (np.median(INCL[np.isfinite(INCL)]), )))
-        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-        ax2.text(0.70, 0.95, textstr, transform=ax2.transAxes, fontsize=14,
-        verticalalignment='top', bbox=props)
-
-        for i,r in enumerate(frange):
-            rect(ax1,r,med[i]-dmed[i],dxr,2*dmed[i])
-            rect(ax2,r,res[i]-dmed[i],dxr,2*dmed[i])
-
-            ax1.hlines(y=med[i],xmin=r,xmax=r+dxr,alpha=0.45,color='blue',lw=1.5)
-            ax2.hlines(y=res[i],xmin=r,xmax=r+dxr,alpha=0.45,color='blue',lw=1.5)
-
-        ax2.axhline(y=0,color='red',alpha=0.45,lw=2.5,zorder=3)
-        ax1.plot(RADI_2,polyex, lw = 2.5 , label = 'Input Curve',color='red',zorder=3)
-        ax1.set_ylabel('Vrot (km/s)')
-        ax1.set_xlabel('Radius (arcsec)')
-
-        ax1.axvline(R_HI5)
-        ax1.legend()
         
-        plt.tight_layout()
-        plt.savefig(namestr+'plot.png',bbox_inches='tight')
-        plt.close()
+        Total_Bias_f = np.sum(Bias_f)
+        Total_Chi_f = np.sum(Chi2_f)
+       
+        xy.append([[Total_Chi,Total_Bias,mass,ba,inc]])
+        xy_f.append([[Total_Chi_f,Total_Bias_f,mass,ba,inc]])
+        if plt1:
+            fig = plt.figure(figsize=(12,8))
+            ax1=fig.add_subplot(2, 2, 1)
+            ax2=fig.add_subplot(2, 2, 2)
+            ax3=fig.add_subplot(2, 2, 3)
+            ax4=fig.add_subplot(2, 2, 4)
+
+            ax1.scatter(R,V, lw = 2.5, label = 'Recovered Curve',
+                    marker='x',alpha=0.25,color='C0',zorder=2)
+            ax2.scatter(res_radi,residuals,color='C0',alpha=0.25,zorder=2)
+            textstr = '\n'.join((
+                r'$\mathrm{Bias=}%.2f$' % (Total_Bias),
+                r'$\chi^2=%.2f$' % (Total_Chi)))
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            ax2.text(0.70, 0.95, textstr, transform=ax2.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+
+            for i,r in enumerate(frange):
+                rect(ax1,r,med[i]-dmed[i],dxr,2*dmed[i])
+                rect(ax2,r,res[i]-dmed[i],dxr,2*dmed[i])
+
+                ax1.hlines(y=med[i],xmin=r,xmax=r+dxr,alpha=0.45,color='blue',lw=1.5)
+                ax2.hlines(y=res[i],xmin=r,xmax=r+dxr,alpha=0.45,color='blue',lw=1.5)
+
+            ax2.axhline(y=0,color='red',alpha=0.45,lw=2.5,zorder=3)
+            ax1.plot(RADI_2,polyex, lw = 2.5 , label = 'Input Curve',color='red',zorder=3)
+            ax1.set_ylabel('Vrot (km/s)')
+            ax1.set_xlabel('Radius (arcsec)')
+            ax2.set_ylabel('Delta Vrot (km/s)')
+            ax2.set_xlabel('Radius (arcsec)')
+
+
+            ax1.legend()
+
+            ax3.scatter(R,V2, lw = 2.5, label = 'Adjusted Curve',
+                    marker='x',alpha=0.25,color='C0',zorder=2)
+            ax3.plot(RADI_2,polyex, lw = 2.5 , label = 'Input Curve',color='red',zorder=3)
+            ax3.set_ylabel('Vrot (km/s)')
+            ax3.set_xlabel('Radius (arcsec)')
+            ax1.axvline(R_HI5)
+            ax2.axvline(R_HI5)
+            ax3.axvline(R_HI5)
+            ax4.axvline(R_HI5)
+
+            ax4.scatter(R,np.sin(I*np.pi/180.),lw = 2.5,
+                    marker='x',alpha=0.25,color='C0',zorder=2)
+            ax4.set_ylabel('Sin(i)')
+            ax4.set_xlabel('Radius (arcsec)')
+            ax4.axhline(np.sin(inc*np.pi/180.))
+            
+            plt.tight_layout()
+            plt.savefig(saveloc+'plot.png',bbox_inches='tight')
+            plt.close()
 
         alldelta_r = np.append(alldelta_r,RADI_med_stats/DHI)
         alldelta = np.append(alldelta,med_residuals)
@@ -258,20 +307,42 @@ med = np.array([])
 dmed = np.array([])
 medR = np.array([])
 
-
-fig=plt.figure(figsize=(12,8))
+fig1,ax1=plt.subplots(figsize=(12,8))
 xy = np.array(xy)
-for i in range(0,len(xy)):
-    if (xy[i,0][0]) < 0 or (xy[i,0][1]) < 0:
-        plt.scatter((xy[i,0][0]),(xy[i,0][1]),color='red')
-    else:
-        plt.scatter((xy[i,0][0]),(xy[i,0][1]),color='blue')
+
+CB1 = plt.scatter((xy[:,0,1]),(xy[:,0,0]),c=xy[:,0,2],cmap='coolwarm')
+cbar = fig1.colorbar(CB1)
+
+x1,x2 = ax1.get_xlim()
+y1,y2 = ax1.get_ylim()
+
+ax1.set_ylim(y1,y2)
+ax1.set_xlim(x1,x2)
+
 plt.xscale('symlog')
 plt.yscale('symlog')
-plt.ylabel('"Bias"',fontsize=21.5)
-plt.xlabel('Chi2',fontsize=21.5)
-plt.savefig('test1.png',bbox_inches='tight')
+plt.xlabel('"Bias"',fontsize=21.5)
+plt.ylabel('Chi2',fontsize=21.5)
+plt.savefig('ChiBias.png',bbox_inches='tight')
 plt.close()
+
+fig2,ax2=plt.subplots(figsize=(12,8))
+xy = np.array(xy_f)
+#for i in range(0,len(xy)):
+CB2 = plt.scatter((xy[:,0,1]),(xy[:,0,0]),c=xy[:,0,2],cmap='coolwarm')
+
+ax2.set_ylim(y1,y2)
+ax2.set_xlim(x1,x2)
+
+cbar = fig2.colorbar(CB2)
+
+plt.xscale('symlog')
+plt.yscale('symlog')
+plt.xlabel('"Bias"',fontsize=21.5)
+plt.ylabel('Chi2',fontsize=21.5)
+plt.savefig('ChiBias_f.png',bbox_inches='tight')
+plt.close()
+
 
 label_size=21.5
 lw=1.5
@@ -330,6 +401,6 @@ plt.ylim(.95*np.min(Y),np.max(Y)*1.05)
 
 #print(medR,med)
 
-plt.savefig('test2.png',bbox_inches='tight')
+plt.savefig('TotalOverlay.png',bbox_inches='tight')
 plt.close()
 #plt.show()
